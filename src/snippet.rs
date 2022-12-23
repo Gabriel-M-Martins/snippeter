cd3762d9b81f2441d7f2f48fdce11e3dc384b9f2
Gabriel M. Martins <gabrielmmartinscontato@gmail.com>
2022-12-23 17:13:14 -0300

    impl fuseable for snippet

--- src/snippet.rs
+++ src/snippet.rs
@@ -1,3 +1,4 @@
+use fuse_rust::{Fuseable, FuseProperty};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,5 +27,30 @@ impl Snippet {
     pub fn value(&mut self, value: impl Into<String>) -> &mut Self {
         self.value = value.into();
         self
+    }
+}
+
+impl Fuseable for Snippet {
+    fn properties(&self) -> Vec<fuse_rust::FuseProperty> {
+        return vec![
+            FuseProperty{value: String::from("name"), weight: 0.6},
+            FuseProperty{value: String::from("desc"), weight: 0.3},
+            FuseProperty{value: String::from("value"), weight: 0.1}
+        ]
+    }
+
+    fn lookup(&self, key: &str) -> Option<&str> {
+        return match key {
+            "name" => Some(&self.name),
+            "desc" => {
+                match &self.desc {
+                    Some(desc) => return Some(desc),
+                    None => return None
+
+                }
+            },
+            "value" => Some(&self.value),
+            _ => None
+        };
     }
 }
